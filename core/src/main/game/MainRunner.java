14a1e71ff840b95b2a546acf99e2c686c0323aab
jack-ellis123 <52923466+jack-ellis123@users.noreply.github.com>
2022-01-27 19:15:25 +0000

    Player Movement
    added player movement in player class, and added very basic rotation (this needs to be fixed)

--- core/src/main/game/MainRunner.java
+++ core/src/main/game/MainRunner.java
@@ -2,37 +2,35 @@ package main.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 
+import main.game.world.player.Player;
+
 public class MainRunner extends ApplicationAdapter {    
-    private static float moveSpeed = 250f;
+    // private static float moveSpeed = 250f;
     
     private OrthographicCamera camera;
     //private SpriteBatch batch;
     private TiledMap worldMap;
     private TiledMapRenderer mapRenderer;
-    private Texture boat;
-    private Sprite player;
+    private Player player;
     private SpriteBatch batch;
-
     @Override
     public void create() {
         worldMap = new TmxMapLoader().load("core/assets/tiles/libmpTest.tmx");
-        boat = new Texture(Gdx.files.internal("core/assets/textures/boat.png"));
+        
 
         camera = new OrthographicCamera();
         batch = new SpriteBatch();
-        player = new Sprite(boat, 32, 64);
+        player = new Player(100,250f);
         mapRenderer = new OrthogonalTiledMapRenderer(worldMap);
 
         camera.setToOrtho(false, 1080, 720);
@@ -40,34 +38,47 @@ public class MainRunner extends ApplicationAdapter {
 
     @Override 
     public void render() {
+        player.render();
         //Clear the screen (1)
         Gdx.gl.glClearColor(0.8f, 0.8f, 1f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        Vector2 pos = new Vector2(0, 0);
-
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) pos.x = -moveSpeed * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) pos.x = moveSpeed * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) pos.y = -moveSpeed * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) pos.y = moveSpeed * Gdx.graphics.getDeltaTime();
-
+        
+        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+            // player.GetPosition().x = -player.GetSpeed() * Gdx.graphics.getDeltaTime();
+            player.RotateBy(-2);
+            camera.rotate(-2);
+        } 
+        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+            
+            // player.GetPosition().x = player.GetSpeed() * Gdx.graphics.getDeltaTime();
+            player.RotateBy(2);
+            camera.rotate(2);
+        } 
+        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            player.GetPosition().y = -player.GetSpeed() * Gdx.graphics.getDeltaTime() * Math.abs(player.GetRotation() / 10);
+            player.GetPosition().x = -player.GetSpeed() * Gdx.graphics.getDeltaTime() * player.GetRotation() / 10;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+            player.GetPosition().y = player.GetSpeed() * Gdx.graphics.getDeltaTime() * Math.abs(player.GetRotation() /10);
+            player.GetPosition().x = player.GetSpeed() * Gdx.graphics.getDeltaTime() * player.GetRotation() / 10;
+        }
+        
         // batch.setProjectionMatrix(camera.combined);
         mapRenderer.setView(camera);
-        camera.translate(pos.x, pos.y);
-
+        camera.translate(player.GetPosition().x, player.GetPosition().y);
         mapRenderer.render();
-
+        
         batch.begin();
-        batch.draw(player, Gdx.graphics.getWidth() / 2 - player.getWidth() / 2, Gdx.graphics.getHeight() / 2 - player.getHeight() / 2);
+        batch.draw(player.GetTextureRegion(), Gdx.graphics.getWidth() / 2 - player.GetSprite().getWidth() / 2, Gdx.graphics.getHeight() / 2 - player.GetSprite().getHeight() / 2,0,0,player.GetSprite().getWidth(),player.GetSprite().getHeight(),2,2,0,false);
         batch.end();
-
+        
         camera.update();
     }
 
     @Override
     public void dispose() {
         worldMap.dispose();
-        boat.dispose();
+        player.GetTexture().dispose();
     }
 
     @Override
--- core/src/main/game/world/player/Player.java
+++ core/src/main/game/world/player/Player.java
@@ -1,8 +1,59 @@
 package main.game.world.player;
 
 import main.game.world.content.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+
 
 public class Player extends Entity {
+
+    private Sprite sprite;
+    private int _health;
+    private float _speed;
+    private Vector2 position = new Vector2(0,0);
+    private Texture boat;
+    private TextureRegion boatregion;
+    private float rotation = 0;
+    
+    public int GetHealth(){
+        return _health;
+    }
+    public float GetSpeed(){
+        return _speed;
+    }
+    public Sprite GetSprite(){
+        return sprite;
+    }
+    public Vector2 GetPosition(){
+        return position;
+    }
+    public Texture GetTexture(){
+        return boat;
+    }
+    public TextureRegion GetTextureRegion(){
+        return boatregion;
+    }
+    public float GetRotation(){
+        return rotation;
+    }
+
+    public void TakeDamage(){}
+    public void Shoot(){}
+
+    public void RotateBy(float deg){
+        rotation += (deg / 2 * Math.PI);
+    }
+    public Player(int health, float speed){
+        boat = new Texture(Gdx.files.internal("core/assets/textures/boat.png"));
+        boatregion = new TextureRegion(boat);
+        sprite = new Sprite(boat, 32, 64);
+        _health = health;
+        _speed = speed;
+    }
 
     @Override
     public void update() {
@@ -10,7 +61,7 @@ public class Player extends Entity {
 
     @Override
     public void render() {
-        
+        position = new Vector2(0,0);
     }
 
     @Override
