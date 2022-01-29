5be7a5b935a3fb79dab2ec2e9991f838f5da5dd7
TomNicho <tomnicho20@gmail.com>
2022-01-29 15:10:36 +0000

    NPC Fixed

--- core/src/main/game/world/content/NPC.java
+++ core/src/main/game/world/content/NPC.java
@@ -2,47 +2,40 @@ package main.game.world.content;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.utils.Disposable;
-import com.badlogic.gdx.utils.TimeUtils;
 
+import main.game.core.Calculations;
 
 public class NPC extends Entity {
     public static final float FIRE_RATE = 1000f;
     public static final float RANGE = 500f;
     public static final float BULLET_SPEED = 250f;
     
-    private Texture NPC;
+    private Texture texture;
     private Sprite sprite;
     private int health;
    
     
-    public NPCActor(int health, Vector2 position,SpriteBatch batch) {
+    public NPC(int health, Vector2 position) {
         this.health = health;
-        this.NPC= new Texture(Gdx.files.internal("core/assets/textures/NPC.png"));    
-        
-       
+        this.texture = new Texture(Gdx.files.internal("textures/NPC.png"));
+        this.sprite = new Sprite(texture);
+
+        sprite.setPosition(position.x, position.y);
     }
     
     public void update() {
-        
-         sprite.setPosition(position.x, position.y);
     }
 
     @Override
-    public void render(SpriteBatch batch,float parentAlpha) {
-        Vector2 project = ActorEventApplicationAdapter.camera.project(new Vector2(npcX, npcY, 0));
-        sprite.draw(npc, project.x, project.y);
-        setPosition(project.x, project.y);
+    public void render(SpriteBatch batch) {
     }
 
     @Override
     public void dispose() {
-        NPC.dispose();
-        
+        this.texture.dispose();
     }
     
     public int getHealth() {
@@ -53,9 +46,7 @@ public class NPC extends Entity {
         return new Vector2(this.sprite.getX(), this.sprite.getY());
     }
     
-     public Vector2 getCenter() {
+    public Vector2 getCenter() {
         return Calculations.SpriteCenter(sprite);
     }
-    
-    
 }
